d616dbe85e2af5d89dca5ccd5338ae2213fd9105
Sam Comer <sam@samtom.xyz>
2021-11-03 20:11:28 +0000

    Set up for radarr file processing.
    Also using Path objects instead of os.path and strings, so file
    management should now be system independent.

--- src/import_subtitles.py
+++ src/import_subtitles.py
@@ -2,9 +2,11 @@ import datetime
 import json
 import os
 
-SCRIPT_PATH = os.path.realpath(__file__)
-SCRIPT_DIR = os.path.dirname(SCRIPT_PATH)
-ENV_FILE = f'{SCRIPT_DIR}\\ARR_ENV.txt'
+from pathlib import Path
+
+SCRIPT_PATH = Path(__file__)
+ENV_FILE = Path(SCRIPT_PATH.parent / 'ARR_ENV.txt')
+NOW = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
 
 def main():
     envs = {}
@@ -21,15 +23,28 @@ def main():
         #  or, something went wrong : )
         if len(envs) == 0:
             envs = {'Custom': 'Test'}
-            log(envs)
+            log_envs(envs)
             raise Exception('No *arr Environment variables were found.')
 
-    log(envs)
+    log_envs(envs)
+
+    if 'RADARR_EVENTTYPE' in envs:
+        if len(envs) > 1: # Not a Test
+            radarr(envs['RADARR_MOVIEFILE_SOURCEFOLDER'],
+                envs['RADARR_MOVIE_PATH'],
+                envs['RADARR_MOVIEFILE_RELATIVEPATH']
+            )
 
 
-def log(envs):
-    with open(f'{SCRIPT_DIR}\\..\\logs\\{datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")}', 'w') as f:
-        f.write(json.dumps(envs))
+def log_envs(envs):
+    with open(SCRIPT_PATH.parent / f'../logs/envs/{NOW}', 'w') as f:
+        f.write(json.dumps(envs, indent=4, sort_keys=True))
+
+
+def radarr(src, dest, title):
+    # Check if src has any SRT files
+    # Import them (all?)
+    pass
 
 
 if __name__ == "__main__":
