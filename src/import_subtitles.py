7b7948501b795a4d1a868990b2664f87fb6daf98
Sam Comer <sam@samtom.xyz>
2021-11-17 14:37:14 +0000

    Removed logging.
    Logging was for personal use, to gather information on what subtitle
    files are named. (Admittedly, this should have been a seperate program
    in the first place).

--- src/import_subtitles.py
+++ src/import_subtitles.py
@@ -1,5 +1,3 @@
-import datetime
-import json
 import os
 import shutil
 
@@ -7,8 +5,6 @@ from pathlib import Path
 
 SCRIPT_PATH = Path(__file__)
 ENV_FILE = Path(SCRIPT_PATH.parent / 'ARR_ENV.txt')
-NOW = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
-log = {}
 
 def main():
     envs = {}
@@ -26,20 +22,15 @@ def main():
         if len(envs) == 0:
             raise Exception('No *arr Environment variables were found.')
 
-    log['envs'] = envs
-
     if 'RADARR_EVENTTYPE' in envs:
         movie = ''
 
         if len(envs) > 1: # Not a Test
-            log['srts'] =  radarr(envs['RADARR_MOVIEFILE_SOURCEFOLDER'],
+            radarr(envs['RADARR_MOVIEFILE_SOURCEFOLDER'],
                 envs['RADARR_MOVIE_PATH'],
                 envs['RADARR_MOVIEFILE_RELATIVEPATH']
             )
             movie = ' ' + envs['RADARR_MOVIE_TITLE']
-
-        with open(SCRIPT_PATH.parent / f'../logs/{NOW} {envs["RADARR_EVENTTYPE"]}{movie}', 'w') as f:
-            f.write(json.dumps(log, indent=4, sort_keys=True))
 
 
 def radarr(src, dest, title):
@@ -48,8 +39,6 @@ def radarr(src, dest, title):
     title = Path(title)
 
     # Get all SRTs
-    to_import = []
-    failed = []
     for parent, _, files in os.walk(src):
         parent = Path(parent)
         for file in files:
@@ -59,18 +48,11 @@ def radarr(src, dest, title):
                 # Only copy files identified as english
                 if 'eng' in file.stem.lower() :
                     to_import.append(parent / file)
-                else:
-                    failed += [file.name]
 
     # Copy each SRT to the destination folder
-    imported = []
     for n, file in enumerate(to_import):
         new_file = Path(dest / f'{title.stem}.eng.{n+1}.srt')
         shutil.copy(file, new_file)
-        imported += [(file.name, new_file.name)]
-
-    # log which are imported
-    return {'failed': failed, 'imported': imported}
 
 
 if __name__ == "__main__":
