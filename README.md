2195c00e509ce3ee9b0e7fbe989f600abcff13dc
Sam Comer <sam@samtom.xyz>
2021-11-20 19:55:04 +0000

    Implemented importing of multiple languages

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ In order to get Radarr to run a python script, I had to use a batch file to call
 It's possible that on any \*nix based systems you may have to use a shell file in a similar way,
 but I only have Radarr running on Windows so I cannot test that.
 
-
 ## Requirements
 
 * Radarr installed
@@ -19,3 +18,4 @@ but I only have Radarr running on Windows so I cannot test that.
 2. Edit `execute.bat` to the correct script location.
 3. Naviage to Sonarr/Radarr [Settings > Connect > + > Custom Script](https://i.imgur.com/UOhYbNf.png), fill in the Path variable to `your\install\path\execute.bat`
 4. Test and save
+5. Edit LANGS in `import_subtitles.py` so that any languages you want to be imported are represented.
--- import_subtitles.py
+++ import_subtitles.py
@@ -5,6 +5,14 @@ import shutil
 
 from pathlib import Path
 
+# Array of tuples in the form ('Language name', 'ISO 639-2/B', 'ISO 639-1')
+# https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+LANGS = [('english', 'eng', 'en')]
+# For example:
+# LANGS = [('english', 'eng', 'en'),
+#     ('french', 'fre', 'fr'),
+#     ('german', 'ger', 'de')]
+
 def main(envs=None):
     # No environment variables provided for testing, use actual variables
     if not envs: 
@@ -19,29 +27,23 @@ def main(envs=None):
 
     src = Path(envs['RADARR_MOVIEFILE_SOURCEFOLDER'])
     dest = Path(envs['RADARR_MOVIE_PATH'])
-    title = Path(envs['RADARR_MOVIEFILE_RELATIVEPATH'])
+    title = Path(envs['RADARR_MOVIEFILE_RELATIVEPATH']).stem
 
     # Get all SRTs
     to_import = []
+    srts = []
     for parent, _, files in os.walk(src):
         parent = Path(parent)
 
         for file in files:
-            file = Path(file)
-
+            file = Path(parent / file)
             if file.suffix != '.srt':
                 pass
 
-            # Only copy files identified as english
-            if 'eng' in file.stem.lower():
-                to_import.append(parent / file)
-            else:
-                pass
+            # Save srt files here
+            srts += [file]
 
-    # Copy each SRT to the destination folder
-    for n, file in enumerate(to_import):
-        new_file = Path(dest / f'{title.stem}.eng.{n+1}.srt')
-        shutil.copy(file, new_file)
+    _process_srts(srts, src, dest, title)
 
 # Checking all potentiall environment variables
 def _get_envs():
@@ -53,6 +55,52 @@ def _get_envs():
                 envs[env] = os.environ[env]
 
     return envs
+
+def _process_srts(srts, src, dest, title):
+    lang_count    = {b: 0 for _,b,_ in LANGS} # {'eng': 0, 'fre': 0}
+    lang_names    = {a: b for a,b,_ in LANGS} # {'english': 'eng', 'french': 'fre'}
+    lang_2_letter = {c: b for _,b,c in LANGS} # {'en': 'eng', 'fr': 'fre'}
+
+    # index of SRTs that have been processed, these are skipped once processed
+    done = []
+
+    # Name matches
+    for lang_name, code in lang_names.items():
+        for n, file in enumerate(srts):
+            if n in done:
+                continue
+
+            if lang_name in file.stem.lower():
+                lang_count[code] += 1
+                done += [n]
+                _copy_file(file, src, dest, title, code, lang_count[code])
+
+    # 3 letter code matches
+    for code in lang_count:
+        for n, file in enumerate(srts):
+            if n in done:
+                continue
+
+            if code in file.stem.lower():
+                lang_count[code] += 1
+                done += [n]
+                _copy_file(file, src, dest, title, code, lang_count[code])
+
+    # 2 letter code matches
+    for lang_2, code in lang_2_letter.items():
+        for n, file in enumerate(srts):
+            if n in done:
+                continue
+
+            if lang_2 in file.stem.lower():
+                lang_count[code] += 1
+                done += [n]
+                _copy_file(file, src, dest, title, code, lang_count[code])
+
+
+def _copy_file(file, src, dest, title, code, count):
+    new_file = Path(dest / f'{title}.{code}.{count}.srt')
+    shutil.copy(src / file, new_file)
 
 
 if __name__ == "__main__":
