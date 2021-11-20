77a1f2ec48784898286798a6324ea06830d05fee
Sam Comer <sam@samtom.xyz>
2021-11-20 16:32:42 +0000

    Removed logging.
    Also made some of the code more pythonic.
    Logic of branching statements are inverted so that failures exit early,
    this is also more readable.
    
    Also removed the radarr function call, now that nothing useful is
    returned with logging removed.

---
+++ import_subtitles.py
@@ -0,0 +1,56 @@
+import os
+import shutil
+
+from pathlib import Path
+
+ENV_FILE = Path(Path(__file__).parent / 'ARR_ENV.txt')
+
+def main():
+    envs = {}
+
+    # Checking all potentiall environment variables
+    with open(ENV_FILE, 'r') as f:
+        for line in f:
+            env = line.rstrip()
+            if env in os.environ:
+                envs[env] = os.environ[env]
+
+    if len(envs) == 0:
+        raise Exception('No *arr environment variables were found')
+
+    if 'RADARR_EVENTTYPE' not in envs:
+        raise Exception('No Radarr environment variables were found')
+
+    # Implies the script was called by Radarr as a Test.
+    if len(envs) == 1:
+        return
+
+    src = Path(envs['RADARR_MOVIEFILE_SOURCEFOLDER'])
+    dest = Path(envs['RADARR_MOVIE_PATH'])
+    title = Path(envs['RADARR_MOVIEFILE_RELATIVEPATH'])
+
+    # Get all SRTs
+    to_import = []
+    for parent, _, files in os.walk(src):
+        parent = Path(parent)
+
+        for file in files:
+            file = Path(file)
+
+            if file.suffix != '.srt':
+                pass
+
+            # Only copy files identified as english
+            if 'eng' in file.stem.lower() :
+                to_import.append(parent / file)
+            else:
+                pass
+
+    # Copy each SRT to the destination folder
+    for n, file in enumerate(to_import):
+        new_file = Path(dest / f'{title.stem}.eng.{n+1}.srt')
+        shutil.copy(file, new_file)
+
+
+if __name__ == "__main__":
+    main()
